1de5419cfb5526f7afbdd43bb6fc630b13cfe1d7
Leo Magalhães <leomagalhaes2008@gmail.com>
2025-03-12 16:19:20 -0300

    added highlighted boolean in Post's schema

--- schemaTypes/blogPost.ts
+++ schemaTypes/blogPost.ts
@@ -55,6 +55,11 @@ export const post = defineType({
             }
         }),
         defineField({
+            name: 'highlighted',
+            title: 'Highlighted',
+            type: 'boolean',
+        }),
+        defineField({
             name: 'content',
             title: 'Content',
             type: 'array',
